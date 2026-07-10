1eb4cb93e4650b8ba47c8cc76d3f13e7f9907964
roundedrectangle <roundedrectangle0@gmail.com>
2026-07-10 13:41:45 +0300

    Add MessagesModel::getMessageIndex

--- src/chat/messagesmodel.h
+++ src/chat/messagesmodel.h
@@ -23,6 +23,7 @@ public:
     Q_INVOKABLE virtual bool clear();
     Q_INVOKABLE virtual void reset();
     Q_INVOKABLE QVariantMap getMessage(int index) const;
+    Q_INVOKABLE int getMessageIndex(qlonglong messageId) const { return messageIndexMap.value(messageId, -1); }
     Q_INVOKABLE QVariantList getMessages(const QVariantList &messageIds) const;
     Q_INVOKABLE QVariantList getMessageIdsForAlbum(qlonglong albumId) const;
     Q_INVOKABLE QVariantList getMessagesForAlbum(qlonglong albumId, int startAt = 0) const;
